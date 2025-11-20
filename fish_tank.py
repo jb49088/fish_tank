4d762c74a75578d1282818d02e84fe03d5203b08
Justin Braun <justinbraun335@gmail.com>
2025-11-20 11:32:20 -0500

    feat: setup project for OOP

--- fish_tank.py
+++ fish_tank.py
@@ -1,28 +1,60 @@
 import shutil
 
-SIZE = shutil.get_terminal_size()
-WIDTH = SIZE.columns
-HEIGHT = SIZE.lines
 
-FISH_TYPES = {
-    "right": r"""
-            \'`.
-             )  \
-(`.??????_.-`' ' '`-.
- \ `.??.`        (o) \_
-  >  ><     (((       (
- / .`??`._      /_|  /'
-(.`???????`-. _  _.-`
-            /__/'
-""",
-    "left": r"""
-       .'`/
-      /  (
-  .-'` ` `'-._??????.')
-_/ (o)        '.??.' /
-)       )))     ><  <
-`\  |_\      _.'??'. \
-  '-._  _ .-'???????'.)
-      `\__\
-""",
-}
+class Settings:
+    """A class to store all settings for fish_tank."""
+
+    def __init__(self):
+        self.size = shutil.get_terminal_size()
+        self.width = self.size.columns
+        self.height = self.size.lines
+
+
+class Fish:
+    """A class to store all information about the fish."""
+
+    FISH_TYPES = [
+        {
+            "right": r"""
+                \'`.
+                 )  \
+    (`.??????_.-`' ' '`-.
+     \ `.??.`        (o) \_
+      >  ><     (((       (
+     / .`??`._      /_|  /'
+    (.`???????`-. _  _.-`
+                /__/'
+    """,
+            "left": r"""
+           .'`/
+          /  (
+      .-'` ` `'-._??????.')
+    _/ (o)        '.??.' /
+    )       )))     ><  <
+    `\  |_\      _.'??'. \
+      '-._  _ .-'???????'.)
+          `\__\
+    """,
+        }
+    ]
+
+    def __init__(self):
+        pass
+
+
+class FishTank:
+    """Overall class to manage fish_tank assets and behavior."""
+
+    def __init__(self):
+        """Initialize fish_tank and create animation resources"""
+        self.settings = Settings()
+        self.fish = Fish()
+
+    def play_animation(self):
+        while True:
+            pass
+
+
+if __name__ == "__main__":
+    fish_tank = FishTank()
+    fish_tank.play_animation()
